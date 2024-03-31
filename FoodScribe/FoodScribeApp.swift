37a1d9a14748e9a8c527095720857f3e3c1d6215
Ryan Morrison <ryanmorrison@gmail.com>
2024-03-31 10:34:23 +0100

    Initial Commit

---
+++ FoodScribe/FoodScribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodScribeApp.swift
+//  FoodScribe
+//
+//  Created by Ryan Morrison on 31/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodScribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
